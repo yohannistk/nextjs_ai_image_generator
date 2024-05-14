0e6c0a919d06566c093d764d00fb7e09f87b59dc
john <onlinejobs2116@gmail.com>
2024-05-14 16:16:49 +0300

    footer component updated

--- app/(landing)/components/footer.tsx
+++ app/(landing)/components/footer.tsx
@@ -3,15 +3,18 @@ import React from "react";
 
 const Fotter = () => {
   return (
-    <div className="bg-gray-800">
-      <div className="max-w-2xl mx-auto text-white py-10">
+    <div className="border-t bg-gray-50">
+      <div className="max-w-2xl mx-auto text-gray-900 py-10">
         <div className="text-center">
-          <h3 className="text-3xl mb-3"> Download our fitness app </h3>
+          <h3 className="text-3xl mb-3 font-semibold">
+            {" "}
+            Download our fitness app{" "}
+          </h3>
           <p> Stay fit. All day, every day. </p>
-          <div className="flex justify-center my-10">
+          <div className="flex flex-col gap-2 items-center md:flex-row justify-center  my-10">
             <Link
               href={""}
-              className="flex items-center border rounded-lg px-4 py-2 w-56 mx-2"
+              className="flex items-center border border-muted-foreground rounded-lg px-4 py-2 w-56 mx-2"
             >
               <img
                 src="https://cdn-icons-png.flaticon.com/512/888/888857.png"
@@ -23,7 +26,7 @@ const Fotter = () => {
             </Link>
             <Link
               href={""}
-              className="flex items-center border rounded-lg px-4 py-2 w-56  mx-2"
+              className="flex items-center border border-muted-foreground rounded-lg px-4 py-2 w-56  mx-2"
             >
               <img
                 src="https://cdn-icons-png.flaticon.com/512/888/888841.png"
@@ -36,18 +39,18 @@ const Fotter = () => {
           </div>
         </div>
         <div className="mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400">
-          <p className="order-2 md:order-1 mt-8 md:mt-0">
+          <p className="order-2 md:order-1 mt-8 md:mt-0 text-gray-700">
             {" "}
             &copy; Pixel Pulse, 2024.{" "}
           </p>
           <div className="order-1 md:order-2">
-            <Link href={"#"} className="px-2 text-muted-foreground">
+            <Link href={"#"} className="px-2 text-gray-700">
               About us
             </Link>
-            <Link href={"#"} className="px-2 border-l text-muted-foreground">
+            <Link href={"#"} className="px-2 border-l text-gray-700">
               Contact us
             </Link>
-            <Link href={"#"} className="px-2 border-l text-muted-foreground">
+            <Link href={"#"} className="px-2 border-l text-gray-700">
               Privacy Policy
             </Link>
           </div>
